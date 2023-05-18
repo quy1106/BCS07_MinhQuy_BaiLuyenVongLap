4330a03a6f04c59111836f0a6dca21c309328a30
minhquy1106 <nngocminhquy@gmail.com>
2023-05-18 22:05:50 +0700

    sửa bài

--- index.js
+++ index.js
@@ -1,20 +1,90 @@
-// bài 2
+/*Bài 1: Tìm số nguyên dương nhỏ nhất sao cho: 1 + 2 + ... n > 1000 
+    */
+   document.getElementById('btnTimSo').onclick=function(){
+    var tong =0;
+   var i = 1;
+   while (tong<=1000) {
+    tong+=i;
+    i++;
+   }
+   document.getElementById('ketQua').innerHTML ='Số cần tìm là: ' + i;
+}
+//  Bài 2: Viết chương trình nhập vào 2 sô x, n tính tổng: S(n)= x + x^2 + x^3 + ... + x^n (Sử dụng vòng lặp và hàm)
 function haiSo () {
+    // input: number
     var x = document.getElementById("so1").value * 1;
     var n = document.getElementById("so2").value * 1;
-
-    console.log(x);
-    console.log(n);
-
-    var tong = 0;
-    var tich = 1;
-
-    for (i=1; i <= n; i++) {
-        tich = tich * x;
-        tong += tich;
-    }
-
-    console.log(tong);
-
-    document.querySelector(".bg-danger").innerHTML = tong;
+// output: number
+var tong = 0;
+var i=1
+while (i<=n) {
+tong = tong + soMu(x,i);
+i++;
 }
+document.getElementById('ketQua2').innerHTML = 'kết quả là: ' + tong;
+}
+
+function soMu(x,n) {
+    var ketQua=x;
+    var i=2;
+    while (i<=n) {
+        ketQua=ketQua*x;
+        i++;
+    }
+    return ketQua;
+}
+// Bài 3: TÍnh giai thừa
+document.getElementById('btnTinhGiaiThua').onclick=function(){
+    // input: number
+    var so = +document.getElementById('nhapSo3').value;
+    // output: number
+    var ketQua3 = 1;
+    for (let i = 1; i <= so; i++) {
+        ketQua3 *= i;
+    }
+    document.getElementById('ketQua3').innerHTML = so + '!= ' + ketQua3;
+}
+// Bài 4: In ra 10 thẻ div
+document.getElementById('btnIn').onclick=function(){
+    // output: string
+    var ketQua4 = '';
+    var i=1;
+    while (i<=10) {
+        if(i%2===0){
+            ketQua4+= '<div class="alert bg-danger w-50 mb-2"></div>'
+        }else{
+            ketQua4 += '<div class="alert bg-primary w-50 mb-2"></div>'
+        }
+        i++;
+    };
+    document.getElementById('ketQua4').innerHTML = ketQua4;
+}
+// Bài 5:
+document.getElementById('btnInSoNT').onclick=function(){
+    // input: number
+    var number = +document.getElementById('nhapSo5').value;
+    // output: string
+    var ketQua5 ='';
+    var soNT ='';
+    for (let i = 2; i <=number; i++) {
+        var checkSNT = kiemTraSNT(i);
+        if (checkSNT) {
+            soNT = i +', ';
+            ketQua5 += soNT;
+        }
+    }
+    var SNT ='';
+    SNT =soNT.replace(',','.');
+    ketQua5=ketQua5.replace(soNT,SNT);
+    document.getElementById('ketQua5').innerHTML ='Các số nguyên tố là: ' + ketQua5;
+}
+function kiemTraSNT(so) {
+    var checkSNT = true;
+    for (let i = 2; i <= Math.sqrt(so); i++) {
+        if(so%i===0){
+            checkSNT =false;
+            break;
+        }
+    };
+    return checkSNT;
+    }
